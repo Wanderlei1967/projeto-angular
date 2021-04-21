7b4dc297371327a8537daf835d939dc14809de3c
Wanderlei <wbolzanjp@gmail.com>
2021-04-21 09:24:15 -0300

    initial commit 2

---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule} from "@angular/forms";
+import {RouterModule} from "@angular/router";
+import {HttpClientModule} from "@angular/common/http";
+
+import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { Erro404Component } from './erro-404/erro-404.component';
+import { CourseModule } from './courses/course.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavBarComponent,
+    Erro404Component
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    CourseModule,
+    RouterModule.forRoot([
+      {
+        path: '', redirectTo: 'courses', pathMatch: 'full'
+      },
+      {
+        path: '**', component: Erro404Component
+      }
+    ])
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
